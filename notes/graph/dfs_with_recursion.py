7c1806d72235070328c6e2b2fef9cbf80331e203
Abdulaziz Musaev <amusaev7225@gmail.com>
2025-12-05 22:05:22 +0100

    Add Depth First Search algorithm implementations for graph

---
+++ notes/graph/dfs_with_recursion.py
@@ -0,0 +1,52 @@
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        # Adjacency list
+        self.adjacency_list = [[] for _ in range(vertices)]
+
+    def add_edge(self, source, destination):
+        self.adjacency_list[source].append(destination)
+        # For an undirected graph
+        self.adjacency_list[destination].append(source)
+
+    def dfs(self, starting_vertex):
+        visited = [False] * self.vertices
+        print("DFS Traversal: ", end="")
+        self.dfs_recursive(starting_vertex, visited)
+
+    def dfs_recursive(self, current_vertex, visited):
+        visited[current_vertex] = True
+        print(current_vertex, end=" ")
+
+        # Recur for all unvisited neighbors
+        for neighbor in self.adjacency_list[current_vertex]:
+            if not visited[neighbor]:
+                self.dfs_recursive(neighbor, visited)
+
+
+class Solution:
+    @staticmethod
+    def main():
+        g = Graph(5)
+
+        g.add_edge(0, 3)
+        g.add_edge(0, 2)
+        g.add_edge(0, 1)
+        g.add_edge(1, 2)
+        g.add_edge(2, 4)
+
+        print("DFS Traversal starting from vertex 0: ", end="")
+        g.dfs(0)
+
+
+if __name__ == "__main__":
+    Solution.main()
+
+
+# V = number of vertices, E = number of edges
+# Time complexity:
+# __init__(): O(V)
+# dfs(): O(V + 2 * E) +==> O(V + E)
+# Space complexity:
+# __init__(): O(V)
+# dfs(): O(V)
---
+++ notes/graph/dfs_with_stack.py
@@ -0,0 +1,37 @@
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        # Adjacency list
+        self.adjacency_list = [[] for _ in range(vertices)]
+
+    def add_edge(self, source, destination):
+        self.adjacency_list[source].append(destination)
+        # For an undirected graph
+        self.adjacency_list[destination].append(source)
+
+    def dfs(self, starting_vertex):
+        visited = [False] * self.vertices
+        stack = []
+
+        stack.append(starting_vertex)
+
+        while stack:
+            current_vertex = stack.pop()
+
+            if not visited[current_vertex]:
+                # Process the current node
+                print(current_vertex, end=" ")
+                visited[current_vertex] = True
+
+            # Push all unvisited neighbors onto the stack
+            for neighbor in self.adjacency_list[current_vertex]:
+                if not visited[neighbor]:
+                    stack.append(neighbor)
+
+# V = number of vertices, E = number of edges
+# Time complexity:
+# __init__(): O(V)
+# dfs(): O(2 * E) +==> O(E)
+# Space complexity:
+# __init__(): O(V + E)
+# dfs(): O(V)
